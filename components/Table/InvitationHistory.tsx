2b878407bef022e9f4a157ec706b5c80060a4647
codingaring <sksms2104@naver.com>
2024-04-22 14:27:30 +0900

    ✨ Feat : add invitations pageNation

--- components/Table/InvitationHistory.tsx
+++ components/Table/InvitationHistory.tsx
@@ -1,6 +1,8 @@
 'use client';
-import { useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import TableLayout from './TableLayout';
+import { getInvitations } from '@/utils/api/getInvitations';
+import PageNationButton from '../PageNation/PageNationButton';
 
 const mockData = [
   {
@@ -17,10 +19,59 @@ const mockData = [
 
 const InvitationHistory = () => {
   const [invitations, setInvitations] = useState([]);
-  const InvitationList = mockData.map(invitation => (
+  const [pageNation, setPageNation] = useState({
+    currentPage: 1,
+    totalPage: 1,
+  });
+
+  const apiQuery = {
+    showCount: 5,
+    dashboardId: 5946,
+  };
+
+  const handleInvitations = async () => {
+    try {
+      const { invitations, totalCount } = await getInvitations({
+        currentPage: pageNation.currentPage,
+        showCount: apiQuery.showCount,
+        dashboardId: apiQuery.dashboardId,
+      });
+      setPageNation(prevState => ({
+        ...prevState,
+        totalPage: Math.ceil(totalCount / apiQuery.showCount),
+      }));
+      setInvitations(invitations);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  const handleCurrentPage = (event: MouseEvent<HTMLButtonElement>) => {
+    if (event.currentTarget.id === 'plus') {
+      if (pageNation.currentPage < pageNation.totalPage) {
+        setPageNation(prevState => ({
+          ...prevState,
+          currentPage: pageNation.currentPage++,
+        }));
+      }
+    } else {
+      if (pageNation.currentPage > 1) {
+        setPageNation(prevState => ({
+          ...prevState,
+          currentPage: pageNation.currentPage--,
+        }));
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleInvitations();
+  }, [pageNation.currentPage]);
+
+  const InvitationList = invitations.map(invitation => (
     <div className='flex justify-between border-solid border-b-[1px] py-4 last:border-none'>
       <div className='flex gap-3 items-center'>
-        <p className='text-base text-tp-black_700 ml-7'>{invitation.email}</p>
+        <p className='text-base text-tp-black_700 ml-7'>{invitation.invitee.email}</p>
       </div>
       <button type='button' className='border border-solid border-tp-gray_700 rounded-lg py-2 px-6 mr-7'>
         버튼대체
@@ -28,7 +79,19 @@ const InvitationHistory = () => {
     </div>
   ));
 
-  return <TableLayout title='초대 내역' tableContent={InvitationList} />;
+  return (
+    <TableLayout
+      title='초대 내역'
+      tableContent={InvitationList}
+      headerContent={
+        <PageNationButton
+          totalPage={pageNation.totalPage}
+          currentPage={pageNation.currentPage}
+          handleCurrentPage={handleCurrentPage}
+        />
+      }
+    />
+  );
 };
 
 export default InvitationHistory;
